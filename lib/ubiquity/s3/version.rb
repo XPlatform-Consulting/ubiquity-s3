ec60fc2b5e44bdb61195b7df05808bb6e829161b
John Whitson <john.whitson@gmail.com>
2014-03-21 14:08:44 -0600

    Bumped to version 1.1.1

--- lib/ubiquity/s3/version.rb
+++ lib/ubiquity/s3/version.rb
@@ -1,7 +1,8 @@
 # => 1.0.0 Initial Release
 # => 1.1.0 Added delete_object and delete_bucket methods
+# => 1.1.1 Fixed typo in gemspec and added additional documentation
 module Ubiquity
   class S3
-    VERSION = '1.1.0'
+    VERSION = '1.1.1'
   end
 end
