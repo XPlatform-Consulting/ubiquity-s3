67f1b336b75045275c50e7ba58b9957667c7a0a9
John Whitson <john.whitson@gmail.com>
2014-04-02 10:16:51 -0600

    Bumped version to 1.2.0

--- lib/ubiquity/s3/version.rb
+++ lib/ubiquity/s3/version.rb
@@ -1,8 +1,9 @@
 # => 1.0.0 Initial Release
 # => 1.1.0 Added delete_object and delete_bucket methods
 # => 1.1.1 Fixed typo in gemspec and added additional documentation
+# => 1.2.0 Added additional Fog::Storage::AWS parameter handling
 module Ubiquity
   class S3
-    VERSION = '1.1.1'
+    VERSION = '1.2.0'
   end
 end
